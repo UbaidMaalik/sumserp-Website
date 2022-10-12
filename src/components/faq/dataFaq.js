874f04e0adc31e36ee331119a9afd18d9ed6cc5f
Ubaid Maalik <umaalik45@gmail.com>
2022-10-12 20:55:10 +0500

    Remove typos

--- src/components/faq/dataFaq.js
+++ src/components/faq/dataFaq.js
@@ -7,15 +7,7 @@ const data = [
     title: "What is SUMS-ERP?",
     des: "SUMS-ERP (Smart University Management System) is a pioneer management system for all educational organizations. SUMS-ERP is much different from another MIS, or ERP’s. There is some factor which makes SUMS-ERP different from other software’s.",
   },
-  {
-    title: "What SUMS can do?",
-    des: "SUMS-ERP (Smart University Management System) can make your organization 80-90% digital. There are online portals from students to director, so in the middle everyone can use their online portals to manage their tasks. All users and their permissions are dynamic so we can manage or change roles and permission accordingly. There are some major tasks SUMS can do!!
-    i-Online Admission & Payment System
-    ii-Online Examination & Learning
-    iii-Connected modules of Staff, Management & Students
-    iv-Generate deep analysis reports
-    v-Access from any device and everywhere",
-  },
+
   {
     title: "Why SUMS-ERP?",
     des: "SUMS-ERP is cloud based web application developed in modern technology. The technology and architecture used by SUMS-ERP makes it different from other applications. And deployment of SUMS-ERP is cloud based, and cloud is more secure and fast then normal hosting’s.",
